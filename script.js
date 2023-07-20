9e82f3f89f11c75155d3568cfb8ee56c821ba77c
ItamarTati <itamar.tati1994@gmail.com>
2023-07-20 22:59:54 +0100

    used pretier

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const sidenavElement = document.querySelector('#mobile-nav');
-    const sidenavOptions = {
-      edge: 'left',
-      draggable: true,
-    };
-    // M is a global variable created in materialize/js/materialize.min.js, which is a bad practice :(
-    const sidenavInstance = M.Sidenav.init(sidenavElement, sidenavOptions);
-    sidenavInstance.close();
+document.addEventListener("DOMContentLoaded", function () {
+  const sidenavElement = document.querySelector("#mobile-nav");
+  const sidenavOptions = {
+    edge: "left",
+    draggable: true,
+  };
+  // M is a global variable created in materialize/js/materialize.min.js, which is a bad practice :(
+  const sidenavInstance = M.Sidenav.init(sidenavElement, sidenavOptions);
+  sidenavInstance.close();
 });
 
 function updateBackgroundHeight() {
-  const getContentSize = document.querySelectorAll('.content')[0].clientHeight;
-  const backgroundElement = document.querySelectorAll('.background')[0];
-  const screenSize = screen.height;
-  const biggerSize = Math.max(getContentSize, screenSize);
-  backgroundElement.style.height = biggerSize + 'px';
+  const getContentSize = document.querySelectorAll(".content")[0].clientHeight;
+  const backgroundElement = document.querySelectorAll(".background")[0];
+  const getScreenSize = screen.height;
+  const getBiggerSize = Math.max(getContentSize, getScreenSize);
+  backgroundElement.style.height = getBiggerSize + "px";
 }
 
-window.addEventListener('load', function() {
-  updateBackgroundHeight(); 
+window.addEventListener("load", function () {
+  updateBackgroundHeight();
 });
 
-window.addEventListener('resize', function() {
+window.addEventListener("resize", function () {
   updateBackgroundHeight();
 });
