be243466da55f76b9e32fb7c80d060c0c7b2db08
ItamarTati <itamar.tati1994@gmail.com>
2023-07-23 07:54:59 +0100

    added image to article page

--- articlePage.js
+++ articlePage.js
@@ -7,7 +7,39 @@ const client = contentful.createClient({
 });
 
 function populatePage(article) {
-    console.log(article)
+    console.log(article.fields)
+    console.log(article.fields.title)
+    console.log(article.fields.description)
+    console.log(article.fields.body)
+    console.log(article.fields.image)
+
+
+  const title = article.fields.title;
+  const description = article.fields.description.content[0].content[0].value;
+  const articleContentElement = document.getElementById('articleContent');
+  
+  const titleElement = document.createElement('h2');
+  titleElement.textContent = title;
+
+  const descriptionElement = document.createElement('blockquote');
+  descriptionElement.textContent = description;
+
+  articleContentElement.appendChild(titleElement);
+  articleContentElement.appendChild(descriptionElement);
+
+    if (article.fields.image && article.fields.image.fields) {
+        const imageLink = 'https:' + article.fields.image.fields.file.url;
+        const figureElement = document.createElement('figure')
+        const imageElement = document.createElement('img');
+        const figcaptionImageElement =  document.createElement('figcaption')
+        imageElement.src = imageLink;
+        imageElement.alt = article.fields.image.fields.title;
+        imageElement.setAttribute('class', 'responsive-img')
+        figcaptionImageElement.textContent = article.fields.image.fields.description
+        articleContentElement.appendChild(figureElement);
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(figcaptionImageElement);
+      }
 }
 
 function getArticle() {
@@ -20,4 +52,5 @@ function getArticle() {
 
 
 getArticle()
+
 
